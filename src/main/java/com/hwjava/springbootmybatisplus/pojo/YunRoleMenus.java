4475f37009c9b398460ea6ca7da8e49a47dea53d
LygHwadeeJava <69786451+LygHwadeeJava@users.noreply.github.com>
2020-08-24 16:49:25 +0800

    Add files via upload

---
+++ src/main/java/com/hwjava/springbootmybatisplus/pojo/YunRoleMenus.java
@@ -0,0 +1,73 @@
+package com.ugaoxin.springbootmybatisplus.pojo;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class YunRoleMenus implements Serializable {
+
+    @TableId(value = "id")
+    @JsonSerialize(using = ToStringSerializer.class)
+    private Long id;
+
+    private Long roleId;
+
+    private Long permissionId;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private String isDelete;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public Long getPermissionId() {
+        return permissionId;
+    }
+
+    public void setPermissionId(Long permissionId) {
+        this.permissionId = permissionId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(String isDelete) {
+        this.isDelete = isDelete == null ? null : isDelete.trim();
+    }
+}
