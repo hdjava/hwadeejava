a5aa6e33f8949d37873bf328e5f009d600e231b5
LygHwadeeJava <[491822650@qq.com](mailto:491822650@qq.com)>
2020-08-25 16:18:44 +0800

    项目初始化

---
+++ src/main/java/com/hwjava/springbootmybatisplus/vo/YunMenusVo.java
@@ -0,0 +1,28 @@
+package com.hwjava.springbootmybatisplus.vo;
+
+import com.hwjava.springbootmybatisplus.pojo.YunMenus;
+
+import java.util.List;
+
+public class YunMenusVo extends YunMenus {
+
+    private   List<YunMenusVo> children;
+    private List<YunMenus> permissionList;
+
+    public List<YunMenus> getPermissionList() {
+        return permissionList;
+    }
+
+    public void setPermissionList(List<YunMenus> permissionList) {
+        this.permissionList = permissionList;
+
+    }
+
+    public List<YunMenusVo> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<YunMenusVo> children) {
+        this.children = children;
+    }
+}
