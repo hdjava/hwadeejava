03aeeef37f77b3072f66c23af09b243dc33aa3ca
zhangwenhao <1803049981@qq.com>
2020-08-25 15:41:16 +0800

    role and charts

---
+++ src/main/java/com/hwjava/springbootmybatisplus/pojo/YunRole.java
@@ -0,0 +1,63 @@
+package com.hwjava.springbootmybatisplus.pojo;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class YunRole implements Serializable {
+
+    @TableId(value = "id")
+    @JsonSerialize(using = ToStringSerializer.class)
+    private Long id;
+
+    private String roleName;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private String isDelete;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName == null ? null : roleName.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(String isDelete) {
+        this.isDelete = isDelete == null ? null : isDelete.trim();
+    }
+}
