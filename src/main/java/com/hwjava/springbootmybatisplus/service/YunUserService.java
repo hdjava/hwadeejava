993e955b1e1c6cabf3c430ad671230f305d46bbc
whn <511172236@qq.com>
2020-08-31 08:31:31 +0800

    用户service层

---
+++ src/main/java/com/hwjava/springbootmybatisplus/service/YunUserService.java
@@ -0,0 +1,8 @@
+package com.hwjava.springbootmybatisplus.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.ugaoxin.springbootmybatisplus.pojo.YunUser;
+
+public interface YunUserService extends IService<YunUser> {
+
+}
