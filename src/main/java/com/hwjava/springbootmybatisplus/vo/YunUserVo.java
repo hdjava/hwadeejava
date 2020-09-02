8895af6c7110bc0da26092a3ce912164b498c365
whn <511172236@qq.com>
2020-09-02 08:39:11 +0800

    whn

---
+++ src/main/java/com/hwjava/springbootmybatisplus/vo/YunUserVo.java
@@ -0,0 +1,38 @@
+package com.hwjava.springbootmybatisplus.vo;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.ugaoxin.springbootmybatisplus.pojo.YunRole;
+import com.ugaoxin.springbootmybatisplus.pojo.YunUser;
+
+import java.util.List;
+import java.util.Set;
+
+public class YunUserVo extends YunUser {
+    private Set<String> menuList;
+    private Set<String> permissionList;
+    @TableField(exist = false)
+    private YunRole role;
+
+    public YunRole getRole() {
+        return role;
+    }
+
+    public void setRole(YunRole role) {
+        this.role = role;
+    }
+    public void setMenuList(Set<String> menuList) {
+        this.menuList = menuList;
+    }
+
+    public void setPermissionList(Set<String> permissionList) {
+        this.permissionList = permissionList;
+    }
+
+    public Set<String> getMenuList() {
+        return menuList;
+    }
+
+    public Set<String> getPermissionList() {
+        return permissionList;
+    }
+}
